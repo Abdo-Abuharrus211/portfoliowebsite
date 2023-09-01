2e738fe3339bf7decd4ed6ac60559fabaacf8a87
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-08-31 21:10:48 -0700

    Add dynamic webpage titles

--- routes/about.js
+++ routes/about.js
@@ -2,8 +2,8 @@ var express = require("express");
 router = express.Router();
 
 module.exports = function (app) {
-    app.get("/about", (req, res) => {
-
-        res.render("about");
-    });
+    app.get('/about', (req, res) => {
+        res.render('about', { pageTitle: 'About' });
+      });      
+      
 }
--- routes/projects.js
+++ routes/projects.js
@@ -4,6 +4,6 @@ router = express.Router();
 module.exports = function(app) {
     
     app.get("/projects", (req, res) => {
-        res.render("projects");
+        res.render("projects", {pageTitle : "Projects | Abdulqadir Abuharrus"});
     })
 }
