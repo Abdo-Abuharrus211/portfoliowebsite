720d331b8734c380303cb641d9fc54be9ed882a4
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-10-01 19:00:30 -0700

    Update Projects page

--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,10 @@
 var express = require("express");
 router = express.Router();
 
-module.exports = function(app) {
-    
+module.exports = function (app) {
+
     app.get("/projects", (req, res) => {
-        res.render("projects", {pageTitle : "Projects | Abdulqadir Abuharrus"});
-    })
+        res.render("projects", { pageTitle: "Projects | Abdulqadir Abuharrus" });
+    });
+
 }
